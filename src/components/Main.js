f7dcbf4ad150abf2e1746b5e425c93090b178b36
Otabek <Askarov9020@gmail.com>
2022-02-27 20:58:03 +0900

    Unnecessary escape character: \[ is fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { InputConatainer, MainContainer, MainWrapper } from "./MainStyle";
 import Header from "./module/Header";
+  /* eslint-disable no-useless-escape */
+
 let emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
